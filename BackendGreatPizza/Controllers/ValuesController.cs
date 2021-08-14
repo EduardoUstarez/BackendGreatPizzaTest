a4425fb33fe7bc428f2682a521dd0ac09363518d
Carlos Angulo <carlos_angulo_az@hotmail.com>
2021-08-13 22:10:19 -0400

    Add project files.

---
+++ BackendGreatPizza/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendGreatPizza.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    [HttpGet]
+    [Route("[action]")]
+    public ActionResult<string> postTest()
+    {
+      return "true";
+    }
+  }
+}
