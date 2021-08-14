2c0d266b562ad4d41af11690c6f5c30df5000c7f
Carlos Angulo <carlos_angulo_az@hotmail.com>
2021-08-14 10:53:27 -0400

    Add entities from postgres database

---
+++ BackendGreatPizza/Entities/Toppings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackendGreatPizza.Entities
+{
+    public partial class Toppings
+    {
+        public long Toppingid { get; set; }
+        public string Description { get; set; }
+    }
+}
